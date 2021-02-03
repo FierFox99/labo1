3fd27dd6bb06fc150fa6c39e6aec5f35a246b6ff
FierFox99 <76745898+FierFox99@users.noreply.github.com>
2021-02-03 11:53:42 -0500

    a

---
+++ labo1/Data/labo1Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using labo1.Models;
+
+namespace labo1.Data
+{
+    public class labo1Context : DbContext
+    {
+        public labo1Context (DbContextOptions<labo1Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<labo1.Models.Class> Class { get; set; }
+    }
+}
